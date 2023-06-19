228c2640325870763680af36094894e315cf127f
BenAjibaAmine <benajiba.mohamedamine@gmail.com>
2023-06-19 11:28:12 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,23 +8,23 @@ int main(void)
 {
 	int n, m, l;
 
-	for (n = 48; n <= 55; n++)
+	for (n = 48; n < 56; n++)
 	{
-	       for (m = n + 1; m <= 56; m++)
-	       {
-	       for (l = m + 1; l <= 57; l++)
-	       {
-	putchar(n);
-putchar(m);
-putchar(l);
-if (n != 55)
-{
-	putchar(',');
-	putchar(' ');
-}
-}
-}
-}
-putchar('\n');
-return (0);
+		for (m = n + 1; m < 57; m++)
+		{
+			for (l = m + 1; l < 58; l++)
+			{
+				putchar(n);
+				putchar(m);
+				putchar(l);
+				if (n != 55)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
